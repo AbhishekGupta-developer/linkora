09b06a5a1a0b9d899fe3b49f11eb79dfd94a8c97
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-09-24 17:34:58 +0530

    Implement registerUser method

--- src/main/java/com/myorganisation/linkora/service/UserServiceImpl.java
+++ src/main/java/com/myorganisation/linkora/service/UserServiceImpl.java
@@ -3,15 +3,49 @@ package com.myorganisation.linkora.service;
 import com.myorganisation.linkora.dto.request.UserRequestDto;
 import com.myorganisation.linkora.dto.response.GenericResponseDto;
 import com.myorganisation.linkora.dto.response.UserResponseDto;
+import com.myorganisation.linkora.entity.User;
+import com.myorganisation.linkora.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    // DEPENDENCY INJECTION USING AUTOWIRED ANNOTATION => NOT RECOMMENDED
+//    @Autowired
+//    private UserRepository userRepository;
+
+
+    // DEPENDENCY INJECTION USING CONSTRUCTOR (PARAMETERISED) INJECTION => RECOMMENDED
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserResponseDto registerUser(UserRequestDto userRequestDto) {
-        return null;
+        User user = new User();
+
+        user.setFirstName(userRequestDto.getFirstName());
+        user.setLastName(userRequestDto.getLastName());
+        user.setGender(userRequestDto.getGender());
+        user.setEmail(userRequestDto.getEmail());
+        user.setPassword(userRequestDto.getPassword());
+
+        userRepository.save(user);
+
+        UserResponseDto userResponseDto = new UserResponseDto();
+
+        userResponseDto.setId(user.getId());
+        userResponseDto.setFirstName(user.getFirstName());
+        userResponseDto.setLastName(user.getLastName());
+        userResponseDto.setGender(user.getGender());
+        userResponseDto.setEmail(user.getEmail());
+
+        return userResponseDto;
     }
 
     @Override
